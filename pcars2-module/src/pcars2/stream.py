2a93e210ef6a107d9999e27c423dab1f7bd8f152
Reis <32417584+felipesdreis@users.noreply.github.com>
2022-08-04 20:24:41 -0300

    atualizacao modulo python
    Uso da biblioteca pcars2

---
+++ pcars2-module/src/pcars2/stream.py
@@ -0,0 +1,36 @@
+from pcars2.packet import Packet
+from io import BytesIO
+from threading import Thread
+import socket
+import struct
+
+
+_MCAST_ANY = "224.0.0.1"
+
+
+class PCarsStreamReceiver(Thread):
+
+    def __init__(self, port=5606):
+        super(PCarsStreamReceiver, self).__init__()
+        self.port = port
+        self.setDaemon(True)
+        self.listeners = []
+
+    def addListener(self, listener):
+        self.listeners.append(listener)
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Bind to the server address
+        sock.bind(("", self.port))
+        # group = socket.inet_aton(_MCAST_ANY)
+        # mreq = struct.pack("4sL", group, socket.INADDR_ANY)
+        # print(socket.IPPROTO_IP)
+        # sock.setsockopt('127.0.0.1', socket.IP_ADD_MEMBERSHIP, mreq)
+
+        while True:
+            data = sock.recv(1500)
+            packet = Packet.readFrom(BytesIO(data))
+            for listener in self.listeners:
+                listener.handlePacket(packet)
